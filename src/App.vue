1460b9a8b09ea92201c9813291904c7bec912f7e
Yuri Anikin <yanikin@voximplant.com>
2018-09-10 17:19:14 +0300

    md-navigation style

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,21 @@
-<template>
-  <div id="app">
-    <md-toolbar class="main-header">
-      <md-button class="md-icon-button" @click="showNavigation = true">
-        <md-icon>menu</md-icon>
-      </md-button>
-      <h3 class="md-title">Default</h3>
-    </md-toolbar>
-    <md-drawer class="main-drawer" :md-active.sync="showNavigation">
-      <md-toolbar class="main-drawer__header" md-elevation="0">
-        <span class="md-title">Default</span>
-      </md-toolbar>
-      <nav id="nav">
-        <md-list  class="md-navigation">
-          <md-list-item class="md-navigation__item">
-            <router-link class="md-navigation__link" to="/">
-              <md-icon>home</md-icon>
-              Home
-            </router-link> 
-          </md-list-item>
-          <md-list-item  class="md-navigation__item">
-            <router-link class="md-navigation__link" to="/about">About</router-link>
-          </md-list-item>
-        </md-list>
-      </nav>
-    </md-drawer>
-    <div class="md-layout md-alignment-top-center">
-      <router-view/>
-    </div>
-  </div>
+<template lang="pug">
+  #app
+    md-toolbar.main-header
+      md-button.md-icon-button(@click='showNavigation = true')
+        md-icon menu
+      h3.md-title Default
+    md-drawer.main-drawer(:md-active.sync='showNavigation')
+      md-toolbar.main-drawer__header(md-elevation='0')
+        span.md-title Default
+      nav.md-navigation
+        router-link.md-navigation__link(to='/')
+          md-icon home
+          | Home
+        router-link.md-navigation__link(to='/about')
+          md-icon forum
+          | About
+    .md-layout.md-alignment-top-center
+      router-view
 </template>
 
 <script lang="ts">
@@ -50,33 +38,43 @@ export default class App extends Vue {
 @import "~vue-material/dist/theme/all"; // Apply the theme
 
 #app {
-  // font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  // -webkit-font-smoothing: antialiased;
-  // -moz-osx-font-smoothing: grayscale;
   .main-drawer {
     background: md-get-palette-color(bluegrey, 800);
+
     &__header {
       background: md-get-palette-color(bluegrey, 900);
+    }
+  }
+  .md-navigation {
+    flex-grow: 1;
+    background: md-get-palette-color(bluegrey, 800);
+
+    &__link {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      font-weight: 500;
+      color :md-get-palette-color(grey, 400); 
+      text-decoration: none;
+      padding: 16px 40px;
+      letter-spacing: 0;
+      opacity: .87;
+
+      .md-icon {
+        font-size: 24px;
+        color: #78909c;
+        margin-left: 0;
+        margin-right: 32px;
+      }
+    }
+    &__link:hover {
+      background-color: #00BCD4;
+    color: #37474F;
     }
   }
   .md-layout {
     margin: 0 auto;
     max-width: 840px;
-  }
-}
-#nav {
-  background: md-get-palette-color(bluegrey, 800);
-  .md-navigation {
-    background: md-get-palette-color(bluegrey, 800);
-    &__link {
-      color :md-get-palette-color(grey, 400); 
-      text-decoration: none;
-      font-weight: 400;
-      letter-spacing: 0;
-      opacity: .87;
-
-      padding: 16px 40px;
-    }
   }
 }
 </style>
